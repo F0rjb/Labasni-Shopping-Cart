33c8b0f2a5c08eb355d58144c60f4292f63e8003
f0rjb <ahmednasrigeo@gmail.com>
2022-12-06 21:15:10 +0100

    even more closer2

--- src/data.js
+++ src/data.js
@@ -1,72 +1,58 @@
 let shopItemsdata = [
-    {
-      id: "jhpfgigcnkdfpnk",
-      name: "Nike Air White Edition",
-      price: 140,
-      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
-      Image: "img/sneakers1.jpg",
-      like:true,
-    },
-    {
-      id: "kpoypongpokmlklk",
-      name: "AF-1",
-      price: 160,
-      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
-      Image: "img/sneakers.jpg",
-      like:false,
-
-    },
-    {
-      id: "kjkjgpkfopknn",
-      name: "YEEZY",
-      price: 189,
-      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
-      Image: "img/sneaker2.jpg",    
-      like:false,
-    },
-    {
-      id: "pkpogkpoknpgnk",
-      name: "Air-Jordan BR Edition",
-      price: 120,
-      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
-      Image: "img/sneakers3.jpg",
-      like:false,
-
-    },
-    {
-      id: "pokhpgocpoknfpkn",
-      name: "BALANCIAGA",
-      price: 250,
-      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
-      Image: "img/sneakers4.jpg",
-      like:false,
-
-    },
-    {
-      id: "pkjponkopfknpkk",
-      name: "Air-Jordan RW Edition",
-      price: 179,
-      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
-      Image: "img/sneakers5.jpg",
-      like:false,
-
-    },
-    {
-      id: "pkogfpnokfpnkpoknpfkh",
-      name: "Vans Walk OFF walls",
-      price: 129,
-      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
-      Image: "img/sneakers6.jpg",
-      like:false,
-
-    },
-    {
-      id: "opfknpofpnkpofkpnk",
-      name: "Nike Free",
-      price: 149,
-      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
-      Image: "img/nike shoe.jpg",
-      like:false,
-
-    },
-  ];
+  {
+    id: "jhpfgigcnkdfpnk",
+    name: "Nike Air White Edition",
+    price: 140,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    Image: "img/sneakers1.jpg",
+  },
+  {
+    id: "kpoypongpokmlklk",
+    name: "AF-1",
+    price: 160,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    Image: "img/sneakers.jpg",
+  },
+  {
+    id: "kjkjgpkfopknn",
+    name: "YEEZY",
+    price: 189,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    Image: "img/sneaker2.jpg",
+  },
+  {
+    id: "pkpogkpoknpgnk",
+    name: "Air-Jordan BR Edition",
+    price: 120,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    Image: "img/sneakers3.jpg",
+  },
+  {
+    id: "pokhpgocpoknfpkn",
+    name: "BALANCIAGA",
+    price: 250,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    Image: "img/sneakers4.jpg",
+  },
+  {
+    id: "pkjponkopfknpkk",
+    name: "Air-Jordan RW Edition",
+    price: 179,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    Image: "img/sneakers5.jpg",
+  },
+  {
+    id: "pkogfpnokfpnkpoknpfkh",
+    name: "Vans Walk OFF walls",
+    price: 129,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    Image: "img/sneakers6.jpg",
+  },
+  {
+    id: "opfknpofpnkpofkpnk",
+    name: "Nike Free",
+    price: 149,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    Image: "img/nike shoe.jpg",
+  },
+];
--- src/main.js
+++ src/main.js
@@ -1,31 +1,25 @@
 let shop = document.getElementById("shop");
 let basket = JSON.parse(localStorage.getItem("data")) || [];
-function changelike (like){
-  return like
-  
-}
 let generateshop = () => {
   return (shop.innerHTML = shopItemsdata
     .map((x) => {
-
-      let { id, name, price, desc, Image,like } = x;
+      let { id, name, price, desc, Image, like } = x;
       let search = basket.find((x) => x.id === id) || [];
-
       return `<div class="item" id="${id}-product" >
         <img width="100%" length="1em" src="${Image}" alt="${name}">
         <div class="details">
             <h3>${name}</h3>
             <p>${desc}</p>
             
-            <img class="heart" onclick="${like}=!${like}" src="${like? 'img/suit-heart-fill.svg' : 'img/suit-heart.svg'}" >
-            
+            <img id="img${id}" product-id="${id}" class="heart" onclick="changelike(event)" src="img/suit-heart.svg" >
+          
 
             <div class="price-quantity">
                 <h2>${price}$</h2>
                 <div class="buttons">
                     <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>                  
                     <div class="quantity" id=${id}>${
-                       search.item === undefined ? 0 : search.item
+        search.item === undefined ? 0 : search.item
       }</div>
                     <i onclick="increment(${id})" class="bi bi-plus-lg"></i>               
                 </div>
@@ -54,23 +48,21 @@ let increment = (id) => {
   //console.log(basket);
   update(selectedItem.id);
   localStorage.setItem("data", JSON.stringify(basket));
-
 };
 
 let decrement = (id) => {
   let selectedItem = id;
   let search = basket.find((x) => x.id === selectedItem.id);
-  if (search===undefined)return;
+  if (search === undefined) return;
   else if (search.item === 0) return;
   else {
     search.item -= 1;
   }
-  
+
   update(selectedItem.id);
-  basket=basket.filter((x)=>x.item!==0);
+  basket = basket.filter((x) => x.item !== 0);
   //console.log(basket);
   localStorage.setItem("data", JSON.stringify(basket));
-
 };
 
 let update = (id) => {
@@ -83,6 +75,30 @@ let calculations = () => {
   let cartIcon = document.getElementById("cartamount");
   cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
 };
+let changelike = (e) => {
+  let iconElm = e.target;
+  iconElm.src = iconElm.src.includes("img/suit-heart.svg")
+    ? "img/suit-heart-fill.svg"
+    : "img/suit-heart.svg";
+  let product = shopItemsdata.find((item) => {
+    return item.id == iconElm.getAttribute("product-id");
+  });
 
+  let currentFav = JSON.parse(localStorage.getItem("fav") || "[]");
+  if (iconElm.src.includes("img/suit-heart.svg")) {
+    let updated = currentFav.filter((item) => {
+      return item.id != iconElm.getAttribute("product-id");
+    });
+    localStorage.setItem("fav", JSON.stringify(updated));
+  } else {
+    localStorage.setItem("fav", JSON.stringify([...currentFav, product]));
+  }
+};
 
 calculations();
+
+let currentFav = JSON.parse(localStorage.getItem("fav") || "[]");
+currentFav.map((item) => {
+  document.querySelector("[product-id='" + item.id + "']").src =
+    "img/suit-heart-fill.svg";
+});
